d5ec8266284059cd9912a755947f36458eb340c0
Aaron Kummer <aaronjkummer@gmail.com>
2022-09-14 18:58:58 -0400

    counts seconds before spawning new enemy

--- gameloop.py
+++ gameloop.py
@@ -4,19 +4,22 @@ from enemy import enemy
 from init import clear
 from events import event_handler
 from grid import drawgrid
-
 def gameloop(pyg):
     # Beginning Game Loop
-    e = enemy(1, 2, [0,200])
+    enemies = []
+    enemyTimer = 0
     while True:
+        if int(pygame.time.get_ticks()/1000) > enemyTimer + 2 :
+            enemyTimer = int(pygame.time.get_ticks()/1000)
+            enemies.append(enemy(1, 2, [0,200]))
+
         pygame.display.update()
         event_handler(pyg)
-
-        clear(pyg)
-        drawgrid(pyg)
-        e.draw(pyg)
-        e.update(pyg)
-
+        for e in enemies:
+            clear(pyg)
+            drawgrid(pyg)
+            e.draw(pyg)
+            e.update(pyg)
         for i,tower in enumerate(pyg['towers']):
             tower.draw(pyg)
         pyg['FramePerSec'].tick(pyg['FPS'])
