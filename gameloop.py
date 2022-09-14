ff30476cdab24ba0ca57ce6d681b653f5147e6da
Aaron Kummer <aaronjkummer@gmail.com>
2022-09-14 19:24:33 -0400

    spawns and updates enemies based on seconds passed

--- gameloop.py
+++ gameloop.py
@@ -1,25 +1,27 @@
-import pygame, sys
+import pygame
+import sys
 from pygame.locals import *
 from enemy import enemy
 from init import clear
 from events import event_handler
 from grid import drawgrid
+
 def gameloop(pyg):
     # Beginning Game Loop
     enemies = []
     enemyTimer = 0
     while True:
-        if int(pygame.time.get_ticks()/1000) > enemyTimer + 2 :
-            enemyTimer = int(pygame.time.get_ticks()/1000)
-            enemies.append(enemy(1, 2, [0,200]))
-
         pygame.display.update()
+        clear(pyg)
+        drawgrid(pyg)
+        if int(pygame.time.get_ticks()/1000) > enemyTimer + 2:
+            enemyTimer = int(pygame.time.get_ticks()/1000)
+            e = enemy(1, 2, [0, 200])
+            enemies.append(e)
         event_handler(pyg)
         for e in enemies:
-            clear(pyg)
-            drawgrid(pyg)
             e.draw(pyg)
             e.update(pyg)
-        for i,tower in enumerate(pyg['towers']):
+        for i, tower in enumerate(pyg['towers']):
             tower.draw(pyg)
         pyg['FramePerSec'].tick(pyg['FPS'])
