c2791d69e8de11f230b65277ea54cabe9bd41783
Aaron Kummer <aaronjkummer@gmail.com>
2022-09-25 22:19:36 -0400

    adds  shoot and find nearest functions, currently only checking x axis

--- enemy.py
+++ enemy.py
@@ -2,9 +2,10 @@ import pygame
 from pathing import heapsolve
 import random
 
+
 def enemy_updater(pyg):
     if not pyg.paused and int(pygame.time.get_ticks()/1000) > pyg.enemyTimer + 1:
-        if len( pyg.enemies ) < 5:
+        if len(pyg.enemies) < 5:
             pyg.enemyTimer = int(pygame.time.get_ticks()/1000)
             e = enemy(1, 2, [0, 0])
             e.generate_path(pyg)
@@ -12,7 +13,7 @@ def enemy_updater(pyg):
         # else:
         #     pyg.enemies[0].randomize_position(pyg)
         #     pyg.enemies[0].generate_path(pyg)
-    for i,e in enumerate(pyg.enemies):
+    for i, e in enumerate(pyg.enemies):
         if e.touchdown == False:
             e.draw(pyg)
             e.draw_path(pyg)
@@ -21,30 +22,33 @@ def enemy_updater(pyg):
         else:
             del pyg.enemies[i]
 
+
 def update_enemy_paths(pyg):
     for e in pyg.enemies:
         e.generate_path(pyg)
+
 
 class enemy():
     def __init__(self, damage, health, pos):
         self.damage = damage
         self.health = health
         self.pos = pos
-        self.size = [5,5]
+        self.size = [5, 5]
         self.touchdown = False
         self.path = []
         self.speed = 0.03
 
     def draw(self, pyg):
-        pyg.grid.draw_circ(pyg, self.pos, [0.5,0.5], 0.5, (255,0,0))
+        pyg.grid.draw_circ(pyg, self.pos, [0.5, 0.5], 0.5, (255, 0, 0))
         # pyg.grid.draw_circ(pyg, self.get_gridpos(), [0.5,0.5], 0.5, (255,0,0))
 
     def get_gridpos(self):
-        r,c = self.pos
+        r, c = self.pos
         return int(c+0.5), int(r+0.5)
 
     def generate_path(self, pyg):
-        self.path = [i[::-1] for i in heapsolve(pyg.grid.grid, self.get_gridpos(), pyg.goal[::-1])]
+        self.path = [
+            i[::-1] for i in heapsolve(pyg.grid.grid, self.get_gridpos(), pyg.goal[::-1])]
         self.prune()
 
     def randomize_position(self, pyg):
@@ -55,8 +59,8 @@ class enemy():
             self.randomize_position(pyg)
 
     def random_walk(self):
-        self.pos[0] += random.uniform(-1,1)*self.speed
-        self.pos[1] += random.uniform(-1,1)*self.speed
+        self.pos[0] += random.uniform(-1, 1)*self.speed
+        self.pos[1] += random.uniform(-1, 1)*self.speed
 
     def distance(self, a, b):
         dx = a[0] - b[0]
@@ -67,13 +71,13 @@ class enemy():
         return self.distance(self.pos, self.path[0])
 
     def velocity(self):
-        if len( self.path ) > 0:
+        if len(self.path) > 0:
             dx = self.path[0][0] - self.pos[0]
             dy = self.path[0][1] - self.pos[1]
             d = abs(dx) + abs(dy)
             if d > 0:
                 return [self.speed*dx/d, self.speed*dy/d]
-        return [-10,-10]
+        return [-10, -10]
 
     def prune(self):
         if self.dist() < 1 and len(self.path) > 1:
@@ -83,8 +87,9 @@ class enemy():
 
     def draw_path(self, pyg):
         if self.path != None:
-            for x,y in self.path:
-                pyg.grid.draw_circ(pyg, [x,y], [0.5,0.5], 0.1, (0,200,0,10))
+            for x, y in self.path:
+                pyg.grid.draw_circ(
+                    pyg, [x, y], [0.5, 0.5], 0.1, (0, 200, 0, 10))
 
     def update(self, pyg):
         if self.path[-1][0] == pyg.goal[0] and self.path[-1][1] == pyg.goal[1]:
@@ -95,7 +100,7 @@ class enemy():
             if len(self.path) > 0:
                 v = self.velocity()
                 if sum(v) > -20 and self.dist() > self.speed:
-                    self.pos[0] +=  v[0] #/ pyg.fpscounter.fps
-                    self.pos[1] +=  v[1] #/ pyg.fpscounter.fps
+                    self.pos[0] += v[0]  # / pyg.fpscounter.fps
+                    self.pos[1] += v[1]  # / pyg.fpscounter.fps
                 if len(self.path) > 1 and self.dist() <= self.speed:
                     self.path = self.path[1:]
--- tower.py
+++ tower.py
@@ -1,10 +1,39 @@
 import pygame
 from enemy import update_enemy_paths
 
+
+class tower():
+    def __init__(self, damage, health, pos, speed, range):
+        self.damage = damage
+        self.health = health
+        self.pos = pos
+        self.speed = speed
+        self.range = range
+
+    def draw(self, pyg):
+        pyg.grid.draw_circ(pyg, self.pos, [0.5, 0.5], 1, (0, 0, 255))
+
+    def shoot(self, pyg, enemy_index):
+        if not pyg.paused and int(pygame.time.get_ticks()/1000):
+            print('shooting enemy')
+            pyg.enemies[enemy_index].health = pyg.enemies[enemy_index].health - self.damage
+            if pyg.enemies[enemy_index].health < 1:
+                print('enemy as been destroyed')
+                del pyg.enemies[enemy_index]
+            # find nearest enemy
+
+    def find_nearest_enemy(self, pyg):
+        for enemy_index, e in enumerate(pyg.enemies):
+            print(self.pos)
+            print(enemy_index)
+            if self.range > (self.pos[0] - pyg.enemies[enemy_index].pos[0]):
+                self.shoot(pyg, enemy_index)
+
+
 def tower_spawner(pyg, gpos):
     if pyg.grid.inbounds(gpos):
         if pyg.grid.grid[gpos[1]][gpos[0]] == 0:
-            pyg.towers += [tower(1, 2, gpos)]
+            pyg.towers += [tower(1, 1, gpos, 1, 2)]
             pyg.grid.grid[gpos[1]][gpos[0]] = 1
             update_enemy_paths(pyg)
         else:
@@ -17,19 +46,8 @@ def tower_spawner(pyg, gpos):
                     update_enemy_paths(pyg)
                     return
 
+
 def tower_updater(pyg):
     for i, tower in enumerate(pyg.towers):
         tower.draw(pyg)
-
-class tower():
-    def __init__(self, damage, health, pos):
-        self.damage = damage
-        self.health = health
-        self.pos = pos
-
-    def draw(self, pyg):
-        pyg.grid.draw_circ(pyg, self.pos, [0.5,0.5], 1, (0,0,255))
-
-    def update(self, pyg):
-        # shoot at enemies? create projectiles?
-        pass
+        tower.find_nearest_enemy(pyg)
